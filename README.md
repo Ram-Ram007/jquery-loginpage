5e6ed38ff44052695e4ba46cfd47df49a157c155
Ram-Ram007 <ramajayam2411@gmail.com>
2023-09-27 01:00:37 +0530

    modified

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Created login form using jquery
 
 # Added validation to check minimum characters in password
+
+## Using Regular expression for validation 
+
+## Using array for validation
--- apps/script.js
+++ apps/script.js
@@ -1,23 +1,74 @@
+// $(document).ready(function () {
+//     const arr = [];
+//     const specialCharacterRegex = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\-]/; 
+//     const btnSubmit = $("#submit");
+    
+//     btnSubmit.click(function (e) {
+//         e.preventDefault();
+        
+//         const email = $("#Email").val();
+//         const password = $("#pass").val();
+        
+//         if (email) {
+//             if (password.length >= 8) {
+//                 if (specialCharacterRegex.test(password)) { 
+//                     const profile = {
+//                         email: email,
+//                         password: password,
+//                     };
+//                     arr.push(profile);
+//                     console.log(JSON.stringify(arr));
+//                 } else {
+//                     alert("Password must contain at least one special character");
+//                 }
+//             } else {
+//                 alert("Password should be at least 8 characters");
+//             }
+//         } else {
+//             alert("Enter a valid email");
+//         }
+//     });
+// });
+
+
+
 $(document).ready(function () {
     const arr = [];
-    btnSubmit = $("#submit");
+    const specialCharacters = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+', '{', '}', '[', ']', ':', ';', '<', '>', ',', '.', '?', '~', '\\', '-'];
+    const btnSubmit = $("#submit");
+
     btnSubmit.click(function (e) {
-        e.preventDefault();  
-      const email = $("#Email").val();
-      const password = $("#pass").val();
-      if(email){
-        if(password.length>=8){
-            const profile = {
-                email: email,
-                password: password,
-              };
-              arr.push(profile);
-              console.log(JSON.stringify(arr));
-        }else{
-            alert(" password should be atleast 8 character")
+        e.preventDefault();
+
+        const email = $("#Email").val();
+        const password = $("#pass").val();
+        let containsSpecialCharacter = false;
+
+        if (email) {
+            if (password.length >= 8) {
+                
+                for (let i = 0; i < password.length; i++) {
+                    if (specialCharacters.includes(password[i])) {
+                        containsSpecialCharacter = true;
+                        break; 
+                    }
+                }
+
+                if (containsSpecialCharacter) {
+                    const profile = {
+                        email: email,
+                        password: password,
+                    };
+                    arr.push(profile);
+                    console.log(JSON.stringify(arr));
+                } else {
+                    alert("Password must contain at least one special character");
+                }
+            } else {
+                alert("Password should be at least 8 characters");
+            }
+        } else {
+            alert("Enter a valid email");
         }
-    }else{
-        alert("enter valid email")
-    }
-  });
+    });
 });
